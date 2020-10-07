8d92926e58addede8b92e1b49b6f3729585a594f
Ares358 <52101053+Ares358@users.noreply.github.com>
2020-10-07 08:41:17 +0530

    Updated higherlower.py
    The program now asks the User the score they wish to achieve. 
    It also shows the view counts of both the videos after submitting the comparison answers.
    Minor changes made in print statements.

--- higherlower.py
+++ higherlower.py
@@ -4,25 +4,31 @@ from youtube_api import YouTubeDataAPI
 
 class Video:
     def __init__(self, yt):
-        terms = open('terms.txt').readlines()
+        terms = open('./terms.txt').readlines()
         mdata = yt.get_video_metadata(yt.search(q=terms[random.randint(0,len(terms)-1)].strip(),max_results=20)[random.randint(0,19)]['video_id'])
         self.title, self.views, self.thumbnail_link, self.creator = mdata['video_title'], int(mdata['video_view_count']), mdata['video_thumbnail'], mdata['channel_title']
+        
 class Game:
-    def loop(self):
+    def loop(self, target):
         score = 0
-        yt = YouTubeDataAPI('YOURAPIKEY')
-        while score < 20:
+        yt = YouTubeDataAPI(YOURAPIKEY)
+        while score < target:
             print('==========================================================================')
-            pv_video = Video(yt)
-            print(f'{pv_video.title} by {pv_video.creator} has {pv_video.views} views.')
-            game_video = Video(yt)
-            print(f'{game_video.title} by {game_video.creator}.')
-            if (input('Higher or Lower?\n').lower() == 'higher') == (game_video.views > pv_video.views):
-                print('Yay')
+            Vid1 = Video(yt)
+            print(f'1. {Vid1.title} by {Vid1.creator} has {Vid1.views} views.')
+            Vid2 = Video(yt)
+            print(f'2. {Vid2.title} by {Vid2.creator}.')
+            if (input('Video #2\'s views compared to Video #1 will be ? (Higher/Lower) ?\n').lower() == 'higher') == (Vid2.views > Vid1.views):
+                print('\nCorrect!\n')
                 score += 1
             else:
-                print('Nah')
+                print('\nIncorrect! Resetting the score!\n')
                 score = 0
+            
+            print(f'{Vid1.creator}\'s video has {Vid1.views} views while {Vid2.creator}\'s video has {Vid2.views}')
             print('SCORE:',score)
-        print('You win!')
-Game().loop()
+            print()
+        print('Congrats! You win!')
+
+target = int(input("Enter the score you want to achieve: "))
+Game().loop(target)
